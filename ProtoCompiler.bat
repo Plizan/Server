113bd02f21593ae1d62bb0786451a1cad4762bda
Plizan <a6698ab@gmail.com>
2020-10-17 01:56:27 +0900

    Origin

---
+++ ProtoCompiler.bat
@@ -0,0 +1,1 @@
+protoc.exe --proto_path=./ --csharp_out=./BufferSource ./Buffer.proto
---
+++ Server/Server/Server/Main.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Protobuf.Buffer;
+
+namespace Server
+{
+    class Main
+    {
+        IPAddress iPAddress = IPAddress.Parse("118.37.68.159");
+
+        public Main(string[] args)
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            {
+
+            }
+        }
+    }
+}
